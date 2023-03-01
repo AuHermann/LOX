83769416849a81a857c42c743d8def21bd87d375
Andreas Hermann <andreashermann170@gmail.com>
2023-03-01 20:38:21 +0100

    initial jlox implementation

---
+++ jlox/app/src/main/java/jlox/TokenType.java
@@ -0,0 +1,18 @@
+package jlox;
+
+enum TokenType {
+
+    // Single char
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // Double
+    BANG, BANG_EQUAL, EQUAL, EQUAL_EQUAL, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL,
+
+    // Litterals
+    IDENTIFIER, STRING, NUMBER,
+
+    // keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR, PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
